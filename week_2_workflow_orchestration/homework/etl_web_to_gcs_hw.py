ac63b9460ce8b9dcdd8a7863c4a3ec16deb1b2fd
jayesh <jayesh423@yahoo.com>
2023-02-05 12:54:47 -0800

    fixes

--- week_2_workflow_orchestration/homework/etl_web_to_gcs_hw.py
+++ week_2_workflow_orchestration/homework/etl_web_to_gcs_hw.py
@@ -32,6 +32,7 @@ def clean(df=pd.DataFrame) -> pd.DataFrame:
 @task()
 def write_local(df: pd.DataFrame, color:str, dataset_file:str) -> Path:
     """ Write Dataframe out locally as parquet file """
+    Path(f"data/{color}").mkdir(parents=True, exist_ok=True)
     path = Path(f"data/{color}/{dataset_file}.parquet")
     df.to_parquet(path, compression="gzip")
     return path
